58eb907b7ef6b45002cd155507f324edbb783765
alex man <alman31247@gmail.com>
2021-11-23 18:01:18 +0300

    node balancer ready

--- test/main.tf
+++ test/main.tf
@@ -1,15 +1,47 @@
-resource "linode_instance" "terraform-web" {
+resource "linode_instance" "web-node" {
+  count = var.nodes_count
+  tags = ["web", "node"]
+
   image = "linode/ubuntu20.04"
-  label = "Terraform-Web-Example"
-  group = "Terraform"
+  label = "web-node-${count.index + 1}"
+  group = "web"
   region = "us-east"
   type = "g6-nanode-1"
   authorized_keys = [ file(var.pubkey) ]
+  private_ip = true
 
   provisioner "local-exec" {
-    command = "ansible-galaxy install -r ./ansible/requirements.yml &&ansible-playbook -u root -i '${self.ip_address},' --private-key ${var.prvkey} ./ansible/main.yml"
+    command = "ansible-galaxy install -r ./ansible/requirements.yml && sleep 30 && ansible-playbook -u root -i '${self.ip_address},' --private-key ${var.prvkey} -e 'proxy_port=${var.proxy_port}' ./ansible/main.yml"
     environment = {
       ANSIBLE_HOST_KEY_CHECKING = "False"
     }
   }
+}
+
+resource "linode_nodebalancer" "web-nbl" {
+    label = "web-nbl"
+    region = "us-east"
+    client_conn_throttle = 20
+    tags = ["web", "nbl"] 
+}
+
+resource "linode_nodebalancer_config" "web-nbl-cfg" {
+    nodebalancer_id = linode_nodebalancer.web-nbl.id
+    port = var.proxy_port
+    protocol = "http"
+    check = "http_body"
+    check_path = "/"
+    check_attempts = 3
+    check_timeout = 30
+    stickiness = "table"
+    algorithm = "leastconn"
+}
+
+resource "linode_nodebalancer_node" "web-nbl-node" {
+    count = var.nodes_count
+    nodebalancer_id = linode_nodebalancer.web-nbl.id
+    config_id = linode_nodebalancer_config.web-nbl-cfg.id
+    address = "${element(linode_instance.web-node.*.private_ip_address, count.index)}:${var.proxy_port}"
+    label = "web-nbl-node"
+    weight = 50
 }
--- test/variables.tf
+++ test/variables.tf
@@ -6,3 +6,11 @@ variable "prvkey" {
   type = string
   default = "./keys/prvkey"
 }
+variable "proxy_port" {
+  type = number
+  default = 3140
+}
+variable "nodes_count" {
+  type = number
+  default = 2
+}
