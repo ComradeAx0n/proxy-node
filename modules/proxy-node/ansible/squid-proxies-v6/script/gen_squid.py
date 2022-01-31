4bb21a73de81e819ca954f093ff65eecc07390e9
alex man <alman31247@gmail.com>
2022-01-31 15:48:07 +0300

    3proxy ready

--- modules/proxy-node/ansible/squid-proxies-v6/script/gen_squid.py
+++ modules/proxy-node/ansible/squid-proxies-v6/script/gen_squid.py
@@ -1,8 +1,12 @@
 # -*- coding: utf-8 -*-
 import argparse
+import crypt
+import grp
 import os
+import pwd
 import string
 import secrets
+import netifaces as ni
 from ipaddress import IPv6Network, IPv6Address
 from random import seed, getrandbits, choices, choice
 
@@ -46,6 +50,7 @@ def add_ipv6(num_ips, unique_ip=1):
     list_ipv6 = []
     network2 = IPv6Network(ipv6_subnet_full)
     list_network2 = list(network2.subnets(new_prefix=64))
+    # netplan_cmd_format = int_ip + "/24"
 
     if os.path.exists(path=sh_add_ip):
         os.remove(path=sh_add_ip)
@@ -59,13 +64,13 @@ def add_ipv6(num_ips, unique_ip=1):
             ipv6 = gen_ipv6(ipv6_subnet=sub)
             list_ipv6.append(str(ipv6))
 
-            cmd = f'ip -6 addr add {ipv6} dev {net_interface}'
+            # netplan_cmd_format = netplan_cmd_format + "," + str(ipv6) + "/128"
 
+            cmd = f'ip -6 addr add {ipv6} dev {net_interface}'
             with open(sh_add_ip, 'a') as the_file:
                 the_file.write(cmd + '\n')
 
     else:
-
         subnet = choices(list_network2, k=10)
 
         for i in range(0, num_ips):
@@ -75,9 +80,16 @@ def add_ipv6(num_ips, unique_ip=1):
             # print(ipv6)
             # r_conn.sadd(pool_name, ipv6)
             # cmd = '/sbin/ifconfig %s inet6 add %s/64' % (net_interface, ipv6)
+            # netplan_cmd_format = netplan_cmd_format + "," + ipv6 + "/128"
             cmd = f'ip -6 addr add {ipv6} dev {net_interface}'
             with open(sh_add_ip, 'a') as the_file:
                 the_file.write(cmd + '\n')
+
+    # netplan_cmd = '''
+    # netplan set ethernets.{net_interface}.addresses=[{netplan_cmd_format}]
+    # netplan apply'''.format(net_interface=net_interface, netplan_cmd_format=netplan_cmd_format)
+    # writeFile(sh_add_ip, netplan_cmd)
+
     return list_ipv6
 
 
@@ -121,7 +133,7 @@ acl to_ipv6 dst ipv6
 http_access deny all !to_ipv6
 {squid_conf_suffix}
 {squid_conf_refresh}
-{block_proxies}
+{block_squidProxies}
 http_access deny all
 
 '''
@@ -168,40 +180,86 @@ http_access allow localhost
 coredump_dir /var/spool/squid3
 '''
 
-proxies = ''''''
+cfg_threeProxy = '''
+daemon
+setuid {uid}
+setgid {gid}
+
+internal {int_ip}
+
+# log /var/log/3proxy/3proxy.log
+# logformat "%Y-%m-%d %H:%M:%S %I %O %U"
+# rotate 10
+
+maxconn 5
+
+auth strong
+users $"/etc/3proxy/3proxy.auth"
+{block_threeProxies}
+'''
+
+if_stat = ni.ifaddresses(net_interface)
+int_ip = ni.ifaddresses(net_interface)[ni.AF_INET][0]['addr']
+print(if_stat[ni.AF_INET] + if_stat[ni.AF_INET6])
+
+threeProxy_uid = pwd.getpwnam("3proxy").pw_uid
+threeProxy_gid = grp.getgrnam("3proxy").gr_gid
+
+squidProxies = ''''''
+threeProxies = ''''''
 ipv6 = add_ipv6(num_ips=number_ipv6, unique_ip=unique_ip)
 
+threeProxies_auth = ''''''
 credsAndIp = 'ip;port;user;password' + '\n'
 
 auth_file = f'/etc/squid/{pool_name}.auth'
 ht = HtpasswdFile(auth_file, new=True)
+i = 0
 
 for ip_out in ipv6:
-    username = ''.join(choice(string.ascii_lowercase + string.digits) for i in range(6))
+    # username = ''.join(choice(string.ascii_lowercase + string.digits) for i in range(6))
+    new_i = '%05d'%(int('00000') + i)
+    username = 'floxys' + new_i
     password = ''.join(secrets.choice(string.ascii_letters + string.digits) for i in range(20))  # for a 20-character password
 
-    proxy_format = '''
+    squidProxy_format = '''
     http_port       {port}
     acl     p{port}  localport       {port}
     acl {user} proxy_auth {user}
     http_access allow {user} p{port}
     tcp_outgoing_address    {ip_out} p{port}
     '''.format(port=start_port, ip_out=ip_out, user=username)
-    proxies += proxy_format + '\n'
+    squidProxies += squidProxy_format + '\n'
+
+    threeProxies_format = '''
+allow {user}
+socks -6 -p{port} -e{ip_out}
+flush
+    '''.format(port=start_port + number_ipv6, ip_out=ip_out, user=username)
+    threeProxies += threeProxies_format + '\n'
+
+    threeProxies_auth_format = '''{user}:CR:{password}'''.format(user=username, password=crypt.crypt(password, crypt.METHOD_MD5))
+    threeProxies_auth += threeProxies_auth_format + '\n'
 
     credsAndIp_format = '''{ip_out};{port};{user};{password}'''.format(port=start_port, ip_out=ip_out, user=username, password=password)
     credsAndIp += credsAndIp_format + '\n'
     
-    start_port = start_port + 1
+    i += 1
+    start_port += 1
     ht.set_password(username, password)
 
 ht.save()
 
 cfg_squid_gen = cfg_squid.format(pid=pool_name, squid_conf_refresh=squid_conf_refresh,
                                  squid_conf_suffix=squid_conf_suffix.format(pid=pool_name),
-                                 block_proxies=proxies)
+                                 block_squidProxies=squidProxies)
+cfg_threeProxy_gen = cfg_threeProxy.format(int_ip=int_ip, uid=threeProxy_uid, gid=threeProxy_gid, block_threeProxies=threeProxies)
 
 writeFile(f'/etc/squid/squid.conf', cfg_squid_gen)
+writeFile(f'/etc/3proxy/3proxy.cfg', cfg_threeProxy_gen)
+os.chown("/etc/3proxy/3proxy.cfg", threeProxy_uid, threeProxy_gid)
+writeFile(f'/etc/3proxy/3proxy.auth', threeProxies_auth)
+os.chown("/etc/3proxy/3proxy.auth", threeProxy_uid, threeProxy_gid)
 writeFile(f'/opt/v6proxies/squid_auth.csv', credsAndIp)
 
 print("=========================== \n")
